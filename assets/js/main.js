be209c6d4bfc11ad5f13dc20bff8e2d03dcd5aed
Lorena Huang Liu <Lo@Lorenas-MacBook-Pro.local>
2018-02-25 18:42:49 -0800

    Changed website template

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Astral by HTML5 UP
+	Strata by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
@@ -8,235 +8,127 @@
 
 	var settings = {
 
-		// Speed to resize panel.
-			resizeSpeed: 600,
+		// Parallax background effect?
+			parallax: true,
 
-		// Speed to fade in/out.
-			fadeSpeed: 300,
-
-		// Size factor.
-			sizeFactor: 11.5,
-
-		// Minimum point size.
-			sizeMin: 15,
-
-		// Maximum point size.
-			sizeMax: 20
+		// Parallax factor (lower = more intense, higher = less intense).
+			parallaxFactor: 20
 
 	};
 
-	var $window = $(window);
+	skel.breakpoints({
+		xlarge: '(max-width: 1800px)',
+		large: '(max-width: 1280px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)'
+	});
 
-	$window.on('load', function() {
+	$(function() {
 
-		skel
-			.breakpoints({
-				desktop: '(min-width: 737px)',
-				mobile: '(max-width: 736px)'
-			})
-			.viewport({
-				breakpoints: {
-					desktop: {
-						width: 1080,
-						scalable: false
-					}
-				}
-			})
-			.on('+desktop', function() {
+		var $window = $(window),
+			$body = $('body'),
+			$header = $('#header'),
+			$footer = $('#footer'),
+			$main = $('#main');
 
-				var	$body = $('body'),
-					$main = $('#main'),
-					$panels = $main.find('.panel'),
-					$hbw = $('html,body,window'),
-					$footer = $('#footer'),
-					$wrapper = $('#wrapper'),
-					$nav = $('#nav'), $nav_links = $nav.find('a'),
-					$jumplinks = $('.jumplink'),
-					$form = $('form'),
-					panels = [],
-					activePanelId = null,
-					firstPanelId = null,
-					isLocked = false,
-					hash = window.location.hash.substring(1);
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
 
-				if (skel.vars.mobile) {
+			$window.on('load', function() {
+				$body.removeClass('is-loading');
+			});
 
-					settings.fadeSpeed = 0;
-					settings.resizeSpeed = 0;
-					$nav_links.find('span').remove();
+		// Touch?
+			if (skel.vars.mobile) {
 
-				}
+				// Turn on touch mode.
+					$body.addClass('is-touch');
 
-				// Body.
-					$body._resize = function() {
-						var factor = ($window.width() * $window.height()) / (1440 * 900);
-						$body.css('font-size', Math.min(Math.max(Math.floor(factor * settings.sizeFactor), settings.sizeMin), settings.sizeMax) + 'pt');
-						$main.height(panels[activePanelId].outerHeight());
-						$body._reposition();
-					};
+				// Height fix (mostly for iOS).
+					window.setTimeout(function() {
+						$window.scrollTop($window.scrollTop() + 1);
+					}, 0);
 
-					$body._reposition = function() {
-						if (skel.vars.mobile && (window.orientation == 0 || window.orientation == 180))
-							$wrapper.css('padding-top', Math.max((($window.height() - (panels[activePanelId].outerHeight() + $footer.outerHeight())) / 2) - $nav.height(), 30) + 'px');
-						else
-							$wrapper.css('padding-top', ((($window.height() - panels[firstPanelId].height()) / 2) - $nav.height()) + 'px');
-					};
+			}
 
-				// Panels.
-					$panels.each(function(i) {
-						var t = $(this), id = t.attr('id');
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
 
-						panels[id] = t;
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
 
-						if (i == 0) {
+		// Footer.
+			skel.on('+medium', function() {
+				$footer.insertAfter($main);
+			});
 
-							firstPanelId = id;
-							activePanelId = id;
+			skel.on('-medium !medium', function() {
+				$footer.appendTo($header);
+			});
 
-						}
-						else
-							t.hide();
+		// Header.
 
-						t._activate = function(instant) {
+			// Parallax background.
 
-							// Check lock state and determine whether we're already at the target.
-								if (isLocked
-								||	activePanelId == id)
-									return false;
+				// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
+					if (skel.vars.browser == 'ie'
+					||	skel.vars.mobile)
+						settings.parallax = false;
 
-							// Lock.
-								isLocked = true;
+				if (settings.parallax) {
 
-							// Change nav link (if it exists).
-								$nav_links.removeClass('active');
-								$nav_links.filter('[href="#' + id + '"]').addClass('active');
+					skel.on('change', function() {
 
-							// Change hash.
-								if (i == 0)
-									window.location.hash = '#';
-								else
-									window.location.hash = '#' + id;
+						if (skel.breakpoint('medium').active) {
 
-							// Add bottom padding.
-								var x = parseInt($wrapper.css('padding-top')) +
-										panels[id].outerHeight() +
-										$nav.outerHeight() +
-										$footer.outerHeight();
-
-								if (x > $window.height())
-									$wrapper.addClass('tall');
-								else
-									$wrapper.removeClass('tall');
-
-							// Fade out active panel.
-								$footer.fadeTo(settings.fadeSpeed, 0.0001);
-								panels[activePanelId].fadeOut(instant ? 0 : settings.fadeSpeed, function() {
-
-									// Set new active.
-										activePanelId = id;
-
-										// Force scroll to top.
-											$hbw.animate({
-												scrollTop: 0
-											}, settings.resizeSpeed, 'swing');
-
-										// Reposition.
-											$body._reposition();
-
-										// Resize main to height of new panel.
-											$main.animate({
-												height: panels[activePanelId].outerHeight()
-											}, instant ? 0 : settings.resizeSpeed, 'swing', function() {
-
-												// Fade in new active panel.
-													$footer.fadeTo(instant ? 0 : settings.fadeSpeed, 1.0);
-													panels[activePanelId].fadeIn(instant ? 0 : settings.fadeSpeed, function() {
-
-														// Unlock.
-															isLocked = false;
-
-													});
-											});
-
-								});
-
-						};
-
-					});
-
-				// Nav + Jumplinks.
-					$nav_links.add($jumplinks).click(function(e) {
-						var t = $(this), href = t.attr('href'), id;
-
-						if (href.substring(0,1) == '#') {
-
-							e.preventDefault();
-							e.stopPropagation();
-
-							id = href.substring(1);
-
-							if (id in panels)
-								panels[id]._activate();
+							$window.off('scroll.strata_parallax');
+							$header.css('background-position', 'top left, center center');
 
 						}
+						else {
 
-					});
+							$header.css('background-position', 'left 0px');
 
-				// Window.
-					$window
-						.resize(function() {
-
-							if (!isLocked)
-								$body._resize();
-
-						});
-
-					$window
-						.on('orientationchange', function() {
-
-							if (!isLocked)
-								$body._reposition();
-
-						});
-
-					if (skel.vars.IEVersion < 9)
-						$window
-							.on('resize', function() {
-								$wrapper.css('min-height', $window.height());
+							$window.on('scroll.strata_parallax', function() {
+								$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
 							});
 
-				// Fix: Placeholder polyfill.
-					$('form').placeholder();
+						}
 
-				// Prioritize "important" elements on mobile.
-					skel.on('+mobile -mobile', function() {
-						$.prioritize(
-							'.important\\28 mobile\\29',
-							skel.breakpoint('mobile').active
-						);
 					});
 
-				// CSS polyfills (IE<9).
-					if (skel.vars.IEVersion < 9)
-						$(':last-child').addClass('last-child');
+					$window.on('load', function() {
+						$window.triggerHandler('scroll');
+					});
 
-				// Init.
-					$window
-						.trigger('resize');
+				}
 
-					if (hash && hash in panels)
-						panels[hash]._activate(true);
+		// Main Sections: Two.
 
-					$wrapper.fadeTo(400, 1.0);
+			// Lightbox gallery.
+				$window.on('load', function() {
 
-			})
-			.on('-desktop', function() {
+					$('#two').poptrox({
+						caption: function($a) { return $a.next('h3').text(); },
+						overlayColor: '#2c2c2c',
+						overlayOpacity: 0.85,
+						popupCloserText: '',
+						popupLoaderText: '',
+						selector: '.work-item a.image',
+						usePopupCaption: true,
+						usePopupDefaultStyling: false,
+						usePopupEasyClose: false,
+						usePopupNav: true,
+						windowMargin: (skel.breakpoint('small').active ? 0 : 50)
+					});
 
-				window.setTimeout(function() {
-					location.reload(true);
-				}, 50);
-
-			});
+				});
 
 	});
 
